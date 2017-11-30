ce924bd089e978ae79c0e77a0beff051f5c62b85
rileyp2000 <rileyp@bishopireton.org>
2017-11-30 12:36:13 -0500

    init

---
+++ Event Driven Computations/src/MailServer.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.TreeSet;
+/**
+ * 
+ * @author rileyp
+ *
+ */
+public class MailServer extends LinkedList<Message>{
+	private TreeSet<Actor> registeredActors;
+	
+	public MailServer() {
+		registeredActors = new TreeSet<Actor>();
+	}
+	
+	public void signUp(Actor actor) {
+		registeredActors.add(actor);
+	}
+
+	public void dispatch(Message msg) {
+		if(msg.getRecipient() != null)
+			msg.getRecipient().receive(msg);
+		else {
+			for(Actor a : registeredActors) 
+				if(!a.getName().equals(msg.getRecipient().getName()))
+					a.receive(msg);			
+		}
+	}
+}
